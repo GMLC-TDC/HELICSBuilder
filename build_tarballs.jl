e36a22fa4dcefe91a626156f75224188752b872c
Dheepak Krishnamurthy <kdheepak89@gmail.com>
2019-02-27 23:00:38 -0700

    Add MacOS

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -1,7 +1,9 @@
 using BinaryBuilder
 
 sources = [
-    "https://github.com/gmlc-tdc/helics-src/archive/v2.0.0-rc1.tar.gz" => "47524ea2558961a6b924a1f972b2d063ca43daa092d91b3b8865ae06f5b33961"
+    "https://github.com/gmlc-tdc/helics-src/archive/v2.0.0-rc1.tar.gz" => "47524ea2558961a6b924a1f972b2d063ca43daa092d91b3b8865ae06f5b33961",
+    "https://dl.bintray.com/boostorg/release/1.68.0/source/boost_1_68_0.tar.bz2" =>
+    "7f6130bc3cf65f56a618888ce9d5ea704fa10b462be126ad053e80e553d6d8b7",
 ]
 
 script = raw"""
@@ -9,8 +11,8 @@ cd $WORKSPACE/srcdir
 cd HELICS-src-2.0.0-rc1
 mkdir build
 cd build
-cmake -DZMQ_USE_STATIC_LIBRARY=ON -DAUTOBUILD_ZMQ=ON -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=/opt/$target/$target.toolchain ..
-make
+cmake -DBOOST_ROOT="../../boost_1_68_0" -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=/opt/$target/$target.toolchain ..
+make -j${nproc}
 make install
 """
 
@@ -19,12 +21,11 @@ products(prefix) = [
 ]
 
 platforms = [
+             MacOS(:x86_64)
              Linux(:x86_64, compiler_abi=CompilerABI(:gcc6))
 ]
 
-dependencies = [
-               "https://github.com/kdheepak/boostBuilder/releases/download/v1.69.0/build_Boost.v1.69.0.jl"
-              ]
+dependencies = []
 
 # Build 'em!
 build_tarballs(
