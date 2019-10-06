29574ca0226cc78b7617cca2f70207e62f38a16b
Dheepak Krishnamurthy <me@kdheepak.com>
2019-10-06 01:05:02 -0600

    Enable all platforms

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -18,17 +18,17 @@ products(prefix) = [
 ]
 
 platforms = [
-             # Linux(:i686, libc=:glibc),
+             Linux(:i686, libc=:glibc),
              Linux(:x86_64, libc=:glibc, compiler_abi=CompilerABI(:gcc8, :cxx11)),
-             # Linux(:aarch64, libc=:glibc),
-             # Linux(:armv7l, libc=:glibc, call_abi=:eabihf),
-             # Linux(:powerpc64le, libc=:glibc),
-             # Linux(:i686, libc=:musl),
-             # Linux(:x86_64, libc=:musl),
-             # Linux(:aarch64, libc=:musl),
-             # Linux(:armv7l, libc=:musl, call_abi=:eabihf),
+             Linux(:aarch64, libc=:glibc),
+             Linux(:armv7l, libc=:glibc, call_abi=:eabihf),
+             Linux(:powerpc64le, libc=:glibc),
+             Linux(:i686, libc=:musl),
+             Linux(:x86_64, libc=:musl),
+             Linux(:aarch64, libc=:musl),
+             Linux(:armv7l, libc=:musl, call_abi=:eabihf),
              MacOS(:x86_64, compiler_abi=CompilerABI(:gcc8, :cxx11)),
-             # FreeBSD(:x86_64),
+             FreeBSD(:x86_64),
              # Windows(:i686),
              Windows(:x86_64, compiler_abi=CompilerABI(:gcc8, :cxx11)),
 ]
