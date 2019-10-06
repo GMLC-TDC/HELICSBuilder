5a8e8a7754ebbc8f6a8c6237bf3860f497734962
Dheepak Krishnamurthy <me@kdheepak.com>
2019-10-06 01:28:16 -0600

    Disable freebsd and windows

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -17,21 +17,7 @@ products(prefix) = [
     LibraryProduct(prefix, "libhelicsSharedLib", :libhelicsSharedLib),
 ]
 
-platforms = [
-             Linux(:i686, libc=:glibc),
-             Linux(:x86_64, libc=:glibc, compiler_abi=CompilerABI(:gcc8, :cxx11)),
-             Linux(:aarch64, libc=:glibc),
-             Linux(:armv7l, libc=:glibc, call_abi=:eabihf),
-             Linux(:powerpc64le, libc=:glibc),
-             Linux(:i686, libc=:musl),
-             Linux(:x86_64, libc=:musl),
-             Linux(:aarch64, libc=:musl),
-             Linux(:armv7l, libc=:musl, call_abi=:eabihf),
-             MacOS(:x86_64, compiler_abi=CompilerABI(:gcc8, :cxx11)),
-             FreeBSD(:x86_64),
-             # Windows(:i686),
-             Windows(:x86_64, compiler_abi=CompilerABI(:gcc8, :cxx11)),
-]
+platforms = supported_platforms()
 
 dependencies = [
                 "https://github.com/benlorenz/boostBuilder/releases/download/v1.71.0/build_boost.v1.71.0.jl",
