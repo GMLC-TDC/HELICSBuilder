94023fcd4c1c676c9c7028311dd19b05f294c7c5
Dheepak Krishnamurthy <me@kdheepak.com>
2019-10-05 23:28:14 -0600

    Update build_tarballs.jl

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -18,8 +18,19 @@ products(prefix) = [
 ]
 
 platforms = [
-             MacOS(:x86_64)
-             Linux(:x86_64, compiler_abi=CompilerABI(:gcc6))
+             # Linux(:i686, libc=:glibc),
+             Linux(:x86_64, libc=:glibc),
+             # Linux(:aarch64, libc=:glibc),
+             # Linux(:armv7l, libc=:glibc, call_abi=:eabihf),
+             # Linux(:powerpc64le, libc=:glibc),
+             # Linux(:i686, libc=:musl),
+             # Linux(:x86_64, libc=:musl),
+             # Linux(:aarch64, libc=:musl),
+             # Linux(:armv7l, libc=:musl, call_abi=:eabihf),
+             MacOS(:x86_64),
+             # FreeBSD(:x86_64),
+             # Windows(:i686),
+             Windows(:x86_64),
 ]
 
 dependencies = [
