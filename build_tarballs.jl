439be28f33c2a779751c2da1afdf3fdf5241030e
Dheepak Krishnamurthy <me@kdheepak.com>
2019-10-05 23:29:52 -0600

    Change compiler abi to gcc6

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -19,7 +19,7 @@ products(prefix) = [
 
 platforms = [
              # Linux(:i686, libc=:glibc),
-             Linux(:x86_64, libc=:glibc),
+             Linux(:x86_64, libc=:glibc, compiler_abi=CompilerABI(:gcc6)),
              # Linux(:aarch64, libc=:glibc),
              # Linux(:armv7l, libc=:glibc, call_abi=:eabihf),
              # Linux(:powerpc64le, libc=:glibc),
@@ -27,10 +27,10 @@ platforms = [
              # Linux(:x86_64, libc=:musl),
              # Linux(:aarch64, libc=:musl),
              # Linux(:armv7l, libc=:musl, call_abi=:eabihf),
-             MacOS(:x86_64),
+             MacOS(:x86_64, compiler_abi=CompilerABI(:gcc6)),
              # FreeBSD(:x86_64),
              # Windows(:i686),
-             Windows(:x86_64),
+             Windows(:x86_64, compiler_abi=CompilerABI(:gcc6)),
 ]
 
 dependencies = [
