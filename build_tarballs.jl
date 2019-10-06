6c782a475af394337e5dbf48917074f0686d16b4
Dheepak Krishnamurthy <me@kdheepak.com>
2019-10-05 23:14:38 -0600

    Update build_tarballs.jl

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -1,17 +1,14 @@
 using BinaryBuilder
 
 sources = [
-    "https://github.com/gmlc-tdc/helics/archive/v2.0.0.tar.gz" => "73e24a09d59ae201e750ac287ab5ceb52c275e4ba6077d47274a9fafc612f2b5",
-    "https://dl.bintray.com/boostorg/release/1.68.0/source/boost_1_68_0.tar.bz2" =>
-    "7f6130bc3cf65f56a618888ce9d5ea704fa10b462be126ad053e80e553d6d8b7",
+    "https://github.com/GMLC-TDC/HELICS/releases/download/v2.2.1/Helics-v2.2.1-source.tar.gz" => "bd78405337aa416aedb1ba9582d4ea3c6e8a9da4918217ff35faaa2f4a63a239",
 ]
 
 script = raw"""
 cd $WORKSPACE/srcdir
-cd HELICS-src-2.0.0-rc1
 mkdir build
 cd build
-cmake -DBOOST_ROOT="../../boost_1_68_0" -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=/opt/$target/$target.toolchain ..
+cmake -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=/opt/$target/$target.toolchain -DBUILD_HELICS_TESTS=OFF ..
 make -j${nproc}
 make install
 """
@@ -25,13 +22,16 @@ platforms = [
              Linux(:x86_64, compiler_abi=CompilerABI(:gcc6))
 ]
 
-dependencies = []
+dependencies = [
+                "https://github.com/benlorenz/boostBuilder/releases/download/v1.71.0/build_boost.v1.71.0.jl",
+                "https://github.com/JuliaInterop/ZMQBuilder/releases/download/v4.2.5%2B5/build.jl",
+]
 
 # Build 'em!
 build_tarballs(
     ARGS,
     "libhelicsSharedLib",
-    v"2.0.0",
+    v"2.2.1",
     sources,
     script,
     platforms,
