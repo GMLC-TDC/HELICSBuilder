69d145088776f6735b18937b762d37cdaf3bcd76
Dheepak Krishnamurthy <kdheepak89@gmail.com>
2019-10-08 10:20:24 -0600

    Update dependencies

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -27,11 +27,51 @@ products(prefix) = [
     LibraryProduct(prefix, "libhelicsSharedLib", :libhelicsSharedLib),
 ]
 
-platforms = supported_platforms()
+platforms = [
+    # Linux(:i686, libc=:glibc, compiler_abi=CompilerABI(:gcc4)),
+    # Linux(:i686, libc=:glibc, compiler_abi=CompilerABI(:gcc7)),
+    Linux(:i686, libc=:glibc, compiler_abi=CompilerABI(:gcc8)),
+    # Linux(:x86_64, libc=:glibc, compiler_abi=CompilerABI(:gcc4)),
+    # Linux(:x86_64, libc=:glibc, compiler_abi=CompilerABI(:gcc7)),
+    Linux(:x86_64, libc=:glibc, compiler_abi=CompilerABI(:gcc8)),
+    # Linux(:aarch64, libc=:glibc, compiler_abi=CompilerABI(:gcc4)),
+    # Linux(:aarch64, libc=:glibc, compiler_abi=CompilerABI(:gcc7)),
+    Linux(:aarch64, libc=:glibc, compiler_abi=CompilerABI(:gcc8)),
+    # Linux(:armv7l, libc=:glibc, call_abi=:eabihf, compiler_abi=CompilerABI(:gcc4)),
+    # Linux(:armv7l, libc=:glibc, call_abi=:eabihf, compiler_abi=CompilerABI(:gcc7)),
+    Linux(:armv7l, libc=:glibc, call_abi=:eabihf, compiler_abi=CompilerABI(:gcc8)),
+    # Linux(:powerpc64le, libc=:glibc, compiler_abi=CompilerABI(:gcc4)),
+    # Linux(:powerpc64le, libc=:glibc, compiler_abi=CompilerABI(:gcc7)),
+    Linux(:powerpc64le, libc=:glibc, compiler_abi=CompilerABI(:gcc8)),
+    # Linux(:i686, libc=:musl, compiler_abi=CompilerABI(:gcc4)),
+    # Linux(:i686, libc=:musl, compiler_abi=CompilerABI(:gcc7)),
+    Linux(:i686, libc=:musl, compiler_abi=CompilerABI(:gcc8)),
+    # Linux(:x86_64, libc=:musl, compiler_abi=CompilerABI(:gcc4)),
+    # Linux(:x86_64, libc=:musl, compiler_abi=CompilerABI(:gcc7)),
+    Linux(:x86_64, libc=:musl, compiler_abi=CompilerABI(:gcc8)),
+    # Linux(:aarch64, libc=:musl, compiler_abi=CompilerABI(:gcc4)),
+    # Linux(:aarch64, libc=:musl, compiler_abi=CompilerABI(:gcc7)),
+    Linux(:aarch64, libc=:musl, compiler_abi=CompilerABI(:gcc8)),
+    # Linux(:armv7l, libc=:musl, call_abi=:eabihf, compiler_abi=CompilerABI(:gcc4)),
+    # Linux(:armv7l, libc=:musl, call_abi=:eabihf, compiler_abi=CompilerABI(:gcc7)),
+    Linux(:armv7l, libc=:musl, call_abi=:eabihf, compiler_abi=CompilerABI(:gcc8)),
+    # MacOS(:x86_64, compiler_abi=CompilerABI(:gcc4)),
+    # MacOS(:x86_64, compiler_abi=CompilerABI(:gcc7)),
+    MacOS(:x86_64, compiler_abi=CompilerABI(:gcc8)),
+    # FreeBSD(:x86_64, compiler_abi=CompilerABI(:gcc4)),
+    # FreeBSD(:x86_64, compiler_abi=CompilerABI(:gcc7)),
+    # FreeBSD(:x86_64, compiler_abi=CompilerABI(:gcc8)),
+    # Windows(:i686, compiler_abi=CompilerABI(:gcc4)),
+    # Windows(:i686, compiler_abi=CompilerABI(:gcc7)),
+    # Windows(:i686, compiler_abi=CompilerABI(:gcc8)),
+    # Windows(:x86_64, compiler_abi=CompilerABI(:gcc4)),
+    # Windows(:x86_64, compiler_abi=CompilerABI(:gcc7)),
+    Windows(:x86_64, compiler_abi=CompilerABI(:gcc8)),
+]
 
 dependencies = [
-                "https://github.com/benlorenz/boostBuilder/releases/download/v1.71.0/build_boost.v1.71.0.jl",
-                "https://github.com/JuliaInterop/ZMQBuilder/releases/download/v4.2.5%2B6/build.jl",
+                "https://github.com/benlorenz/boostBuilder/releases/download/v1.71.0-1/build_boost.v1.71.0.jl",
+                "https://github.com/kdheepak/ZMQBuilder/releases/download/v4.3.1/build_ZMQ.v4.3.1.jl",
 ]
 
 # Build 'em!
