1719f7bac5c891ea79ea0d836723f6ebd4981355
Ryan Mast <nightlark@users.noreply.github.com>
2020-03-03 19:26:20 -0800

    Move tarball SHA to its own line

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -1,11 +1,12 @@
 using BinaryBuilder
 
 HELICS_VERSION = v"2.4.0"
+HELICS_SHA = "8de39728c7bb03be0bde0d506acc827bea732eddb7bb46892027b777b10dab27"
 
 sources = [
     FileSource(
         "https://github.com/GMLC-TDC/HELICS/releases/download/v$HELICS_VERSION/Helics-v$HELICS_VERSION-source.tar.gz",
-        "8de39728c7bb03be0bde0d506acc827bea732eddb7bb46892027b777b10dab27",
+        "$HELICS_SHA",
     ),
 ]
 
