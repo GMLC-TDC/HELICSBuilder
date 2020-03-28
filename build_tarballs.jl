609fc1ab0071e01aa883bcb96013a54aab1740b9
Ryan Mast <nightlark@users.noreply.github.com>
2020-03-28 09:29:17 -0700

    Update Julia package to 2.4.2

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -1,7 +1,7 @@
 using BinaryBuilder
 
-HELICS_VERSION = v"2.4.1"
-HELICS_SHA = "ac077e9efe466881ea366721cb31fb37ea0e72a881a717323ba4f3cdda338be4"
+HELICS_VERSION = v"2.4.2"
+HELICS_SHA = "957856f06ed6d622f05dfe53df7768bba8fe2336d841252f5fac8345070fa5cb"
 
 sources = [
     ArchiveSource(
